d5284a4551d6ac308c95e9fa3700cdefbc806910
EdwinP12 <peraltae4@newpaltz.edu>
2021-10-04 15:16:13 -0400

    vue/user made servicses folder

---
+++ client/src/assets/components/LoginBadge.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="buttons" v-if="!user">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" @click="login">
+            Log in
+          </a>
+        </div>
+        <div v-else>
+            Hello {{name}}
+        </div>
+</template>
+
+<script>
+import Session, { Login } from "../services/session";
+
+
+export default {
+    data: ()=>({
+        user: Session.user
+    }),
+    methods: {
+        login(){
+            console.log(Session)
+            Login();
+           
+            console.log(Session)
+        }
+    },
+    computed:{
+        name(){
+            return this.user.FirstName + '' + this.user.LastName;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
