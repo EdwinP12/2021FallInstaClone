aeb3b1face4402b0967e8d5b0ed52c1a1512a71c
EdwinP12 <peraltae4@newpaltz.edu>
2021-10-18 14:49:59 -0400

    messages component

--- client/src/services/session.js
+++ client/src/services/session.js
@@ -3,14 +3,23 @@ import { Login } from "./users";
 
 const session = {
     user: null,
-    messages: [],
+    messages: [ 
+        { text:"Hello World", type: "danger"},
+        { text:"Hello New Paltz", type: "success"},
+    ],    //{text: string, type: string }
     toRoute: '/feed',
     Login(handle, password){
-        const response = Login(handle, password);
+
+        try{
+            const response = Login(handle, password);
         
         this.user = response.user;
 
-        router.push(this.toRoute);
+        router.push(this.toRoute)
+        }catch (error){
+            this.messages.push({text: error.msg, type: 'warning'})
+        }
+        
     }
 };
 
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="section">
-      <h1 class="title">
-          Feed Page
-      
-      </h1>
+      <h1 class="title">Feed Page</h1>
       <div class="columns">
             <div class="column is-one-third is-offset-one-third">
 
@@ -18,11 +15,13 @@
 </template>
 
 <script>
-import Post from "../components/Post.vue"
+import Post from "../components/Post.vue";
 import session from "../services/session";
 import { GetWall } from "../services/posts";
 export default {
-  components: { Post },
+  components: { 
+    Post 
+    },
   data: ()=> ({
       posts: GetWall(session.user.handle)
   })
